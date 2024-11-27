b82c1399fa70e792c48ca2fb7e7ad126bb191277
hellijah <thierry.corlieto@gmail.com>
2024-11-27 21:27:43 +0100

    stripe working but payment in cart while waiting for order branche to merge

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,39 +1,36 @@
 class CheckoutController < ApplicationController
   def create
-    @total = params[:total].to_d
+    @total = (params[:total].to_d * 100).to_i # Convertir en centimes
+
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
             currency: 'eur',
-            unit_amount: (@total*100).to_i,
+            unit_amount: @total, # Montant en centimes
             product_data: {
               name: 'Rails Stripe Checkout',
             },
-            unit_amount: @total,
           },
           quantity: 1,
         }
       ],
       mode: 'payment',
-      # metadata: {},
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
-    redirect_to @session.url, allow_other_host: true
+    redirect_to session.url, allow_other_host: true
   end
 
   def success
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    # @event_id = @session.metadata.event_id
 
-    # Attendance.create(user: current_user, event_id: @event.id, stripe_customer_id: @session.customer )
-    # redirect_to event_path(@event_id), notice: "Paiement réussi ! Vous êtes inscrit à l'événement."
+    # Logique supplémentaire à implémenter si nécessaire
   end
 
   def cancel
-    # Optional
+    # Logique à ajouter en cas d'annulation
   end
 end
