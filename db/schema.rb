33058831c332af6ac93792095276efefdc2e99be
AkaKwak <christophe.alaterre@gmail.com>
2024-11-27 10:34:37 +0100

    do not use

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_27_105720) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_25_162328) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "item_id", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_27_105720) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
