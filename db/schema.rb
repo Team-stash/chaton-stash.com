0e5e6f8983975d6bd08da77d3ab6adab58491165
AkaKwak <christophe.alaterre@gmail.com>
2024-11-27 11:17:31 +0100

    fmigration started

--- db/schema.rb
+++ db/schema.rb
@@ -68,11 +68,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_27_105720) do
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
